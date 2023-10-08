faab05e21750b12d5f47e58c53e9841048d87a66
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2023-10-08 20:01:58 +0700

    Fix Dictionary cells

--- Scripts/shared.cs
+++ Scripts/shared.cs
@@ -5,33 +5,26 @@ public partial class shared : Node
 {
 	enum Tetromino
 	{
-		I, O, T, J, L, S
+		I, O, T, J, L, S, Z
 	}
 
 	private Dictionary<Tetromino, List<Vector2>> cells;
-	Dictionary<Tetromino, string> citiesq;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		// cells = new Dictionary<Tetromino, List<Vector2>>(){
-		// 	{Tetromino.I,List}
-		// }
-		citiesq = new Dictionary<Tetromino, string>(){
-			{Tetromino.I, "London, Manchester, Birmingham"},
-			{Tetromino.O, "Chicago, New York, Washington"},
-			{Tetromino.T, "Mumbai, New Delhi, Pune"}
-		};
-		var bigCities = new List<Vector2>()
-		{
-			new Vector2(-1,0),
-			new Vector2(0,0),
-			new Vector2(1,0),
-			new Vector2(2,0)
+		cells = new Dictionary<Tetromino, List<Vector2>>(){
+			{Tetromino.I,new List<Vector2>(){new Vector2(-1,0),new Vector2(0,0),new Vector2(1,0),new Vector2(2,0)}},
+			{Tetromino.J,new List<Vector2>(){new Vector2(-1,1),new Vector2(-1,0),new Vector2(0,0),new Vector2(1,0)}},
+			{Tetromino.L,new List<Vector2>(){new Vector2(1,1),new Vector2(-1,0),new Vector2(0,0),new Vector2(1,0)}},
+			{Tetromino.O,new List<Vector2>(){new Vector2(0,1),new Vector2(1,1),new Vector2(0,0),new Vector2(1,0)}},
+			{Tetromino.S,new List<Vector2>(){new Vector2(0,1),new Vector2(1,1),new Vector2(-1,0),new Vector2(0,0)}},
+			{Tetromino.T,new List<Vector2>(){new Vector2(0,1),new Vector2(-1,0),new Vector2(0,0),new Vector2(1,0)}},
+			{Tetromino.Z,new List<Vector2>(){new Vector2(-1,1),new Vector2(0,1),new Vector2(0,0),new Vector2(1,0)}}
 		};
 		GD.Print("baru11");
-		GD.Print(bigCities.GetType());
-		GD.Print(bigCities);
+		GD.Print(cells.GetType());
+		GD.Print(cells);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.

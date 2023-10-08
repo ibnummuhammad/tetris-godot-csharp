eb4ba09eea400c4c01589eae439c059120a4e441
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2023-10-08 19:46:33 +0700

    Fix List

--- Scripts/shared.cs
+++ Scripts/shared.cs
@@ -8,19 +8,30 @@ public partial class shared : Node
 		I, O, T, J, L, S
 	}
 
-	private Dictionary<Tetromino, List<Vector2>> cells = new();
+	private Dictionary<Tetromino, List<Vector2>> cells;
 	Dictionary<string, string> citiesq;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		// cells = new Dictionary<Tetromino, List<Vector2>>(){
+		// 	{Tetromino.I,List}
+		// }
 		citiesq = new Dictionary<string, string>(){
 			{"UK", "London, Manchester, Birmingham"},
 			{"USA", "Chicago, New York, Washington"},
 			{"India", "Mumbai, New Delhi, Pune"}
 		};
-		GD.Print("disiniffbbb");
-		GD.Print(citiesq.GetType());
+		var bigCities = new List<string>()
+		{
+			"New York",
+			"London",
+			"Mumbai",
+			"Chicago"
+		};
+		GD.Print("disitu");
+		GD.Print(bigCities.GetType());
+		GD.Print(bigCities);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.

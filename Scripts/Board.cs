138cdf4f7237362f6c66b764e75b5693f70a3126
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2023-10-14 15:47:00 +0700

    Add properties

--- Scripts/Board.cs
+++ Scripts/Board.cs
@@ -1,10 +1,15 @@
 using Godot;
-using System;
+using System.Collections.Generic;
 
 public partial class Board : Node
 {
 	[Export]
 	private PackedScene tetrominoScene;
+
+	private int columnCount = 10;
+	private int rowCount = 20;
+	private Shared.Tetromino nextTetromino;
+	private List<Shared.Tetromino> tetrominos;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()

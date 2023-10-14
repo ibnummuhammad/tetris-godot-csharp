931cda194d5537b5c446b3fe0fc229b9804bd653
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2023-10-14 15:39:23 +0700

    Add SpawnTetromino()

--- Scripts/Board.cs
+++ Scripts/Board.cs
@@ -3,6 +3,9 @@ using System;
 
 public partial class Board : Node
 {
+	[Export]
+	private PackedScene tetrominoScene;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -10,6 +13,10 @@ public partial class Board : Node
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
+	{
+	}
+
+	public void SpawnTetromino(Shared.Tetromino type)
 	{
 	}
 }

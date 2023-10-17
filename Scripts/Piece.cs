b3f9a964e7790189f2669349bee36c7ed43db0ca
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2023-10-17 22:18:59 +0700

    Add GetNode() for Sprite2D and CollisionShape2D

--- Scripts/Piece.cs
+++ Scripts/Piece.cs
@@ -3,9 +3,14 @@ using System;
 
 public partial class Piece : Area2D
 {
+	private Sprite2D sprite2D;
+	private CollisionShape2D collisionShape2D;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		sprite2D = GetNode<Sprite2D>("Sprite2D");
+		collisionShape2D = GetNode<CollisionShape2D>("CollisionShape2D");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
